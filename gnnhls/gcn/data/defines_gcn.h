c279e61ea3ca71d2b6f436dcaa0cae31cd5f6189
Joseph Xiao <55932881+josephxiao8@users.noreply.github.com>
2026-03-16 18:26:44 -0400

    artifacts 2

--- gnnhls/gcn/data/defines_gcn.h
+++ gnnhls/gcn/data/defines_gcn.h
@@ -1,4 +1,4 @@
-// Graph dataset: OGBG-MOLHIV
+// Graph dataset: OGBN-ARXIV
 // GNN model name: GCN
 
 #ifndef DEFINES_GCN_H
@@ -6,10 +6,10 @@
 #define DEFINES_GCN_H
 
 //Graph Info
-#define N_NODES 1049163
-#define N_EDGES 2259376
+#define N_NODES 169343
+#define N_EDGES 1166243
 
-#define MAX_IN_DEG 10
+#define MAX_IN_DEG 13155
 #define MIN_IN_DEG 0
 
 // GNN Layer Model Info
